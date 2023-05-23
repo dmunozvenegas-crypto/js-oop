a20ac324c3b16b314f3634292e83c15d6bdf6e59
kali <kali@kali.edu>
2023-05-22 20:14:53 -0500

    make first instance of cat

---
+++ oop-js/OOP-P/04-What-are-you.js
@@ -0,0 +1,16 @@
+/*Using the code from the previous exercise, add a constructor method that logs to the console I'm a cat! when a new Cat object is initialized.
+class Cat {
+
+}
+
+let kitty = new Cat(); */
+
+
+class Cat {
+	constructor(){
+		console.log("I'm a Cat!")
+	}
+}
+
+let kitty = new Cat();
+
