e3e85cb387e71aac403399ef2741bf94aad983aa
kali <kali@kali.edu>
2023-05-22 23:25:04 -0500

    Adding easy these are actual problems that need to be solved.

---
+++ oop-js/OOP-P/easy-oop/01-rectangles.js
@@ -0,0 +1,12 @@
+/*Create a class Rectangle.
+
+The constructor should take 2 arguments which represent width and length respectively.
+
+Implement the class so that the output from the example below is correct.
+
+let rect = new Rectangle(4, 5);
+
+console.log(rect.getWidth()); // 4
+console.log(rect.getLength()); // 5
+console.log(rect.getArea()); // 20
+*/
