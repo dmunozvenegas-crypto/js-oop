f5223bd77ed17bdcd9e90eb457721cd9400b2b93
kali <kali@kali.edu>
2023-05-22 21:19:22 -0500

    Adding all files with only questions set 2.

---
+++ oop-js/OOP-P/2nd-ex/02-start-the-engine.js
@@ -0,0 +1,16 @@
+/*Change the following code so that creating a new Truck automatically invokes startEngine.
+
+class Vehicle {
+  constructor(year) {
+    this.year = year;
+  }
+}
+
+class Truck extends Vehicle {
+  startEngine() {
+    console.log('Ready to go!')
+  }
+}
+
+let truck = new Truck(2003);
+console.log(truck.year); // 2003 */
---
+++ oop-js/OOP-P/2nd-ex/08-towable-2.js
@@ -0,0 +1,26 @@
+/*
+ 
+Towable (part 2)
+
+Using the following code, create a class named Vehicle that, upon instantiation, assigns the passed in argument to year property. Both Truck and Car should inherit from Vehicle.
+
+const towMixin = {
+  tow() {
+    return "I can tow a trailer!";
+  }
+}
+
+class Truck {
+  constructor() {
+    Object.assign(this, towMixin);
+  }
+}
+
+class Car {}
+
+let truck = new Truck(2002);
+console.log(truck.year);
+console.log(truck.tow());
+
+let car = new Car(2015);
+console.log(car.year); */
